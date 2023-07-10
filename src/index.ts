b977e3c48cbd331d851b5db3467629613c6d9db9
Sean Nilan <snilan@method.actor>
2023-07-10 13:46:24 -0400

    refactor: make alias Type -> T to improve readability

--- src/index.ts
+++ src/index.ts
@@ -1,225 +1,219 @@
-import { Static, Type } from "@sinclair/typebox"
+import { Type as T } from "@sinclair/typebox"
 import { promises as fs } from "node:fs"
 
-const Route = Type.Union([
-  Type.String(),
-  Type.Object({
-    pattern: Type.String(),
-    zone_id: Type.String(),
-    custom_domain: Type.Optional(Type.Boolean()),
+const Route = T.Union([
+  T.String(),
+  T.Object({
+    pattern: T.String(),
+    zone_id: T.String(),
+    custom_domain: T.Optional(T.Boolean()),
   }),
-  Type.Object({
-    pattern: Type.String(),
-    zone_name: Type.String(),
-    custom_domain: Type.Optional(Type.Boolean()),
+  T.Object({
+    pattern: T.String(),
+    zone_name: T.String(),
+    custom_domain: T.Optional(T.Boolean()),
   }),
-  Type.Object({
-    pattern: Type.String(),
-    custom_domain: Type.Optional(Type.Boolean()),
+  T.Object({
+    pattern: T.String(),
+    custom_domain: T.Optional(T.Boolean()),
   }),
 ])
 
-const Rule = Type.Object({
-  type: Type.Union([
-    Type.Literal("ESModule"),
-    Type.Literal("CommonJS"),
-    Type.Literal("CompiledWasm"),
-    Type.Literal("Text"),
-    Type.Literal("Data"),
+const Rule = T.Object({
+  type: T.Union([
+    T.Literal("ESModule"),
+    T.Literal("CommonJS"),
+    T.Literal("CompiledWasm"),
+    T.Literal("Text"),
+    T.Literal("Data"),
   ]),
-  globs: Type.Array(Type.String()),
-  fallthrough: Type.Optional(Type.Boolean()),
+  globs: T.Array(T.String()),
+  fallthrough: T.Optional(T.Boolean()),
 })
 
-const CustomBuild = Type.Object({
-  command: Type.String(),
-  cwd: Type.Optional(Type.String()),
-  watch_dir: Type.Optional(
-    Type.Union([Type.String(), Type.Array(Type.String())])
+const CustomBuild = T.Object({
+  command: T.String(),
+  cwd: T.Optional(T.String()),
+  watch_dir: T.Optional(T.Union([T.String(), T.Array(T.String())])),
+})
+
+const DevelopmentSettings = T.Object({
+  ip: T.Optional(T.String()),
+  port: T.Optional(T.Number()),
+  local_protocol: T.Optional(
+    T.Union([T.Literal("http"), T.Literal("https")])
   ),
-})
-
-const DevelopmentSettings = Type.Object({
-  ip: Type.Optional(Type.String()),
-  port: Type.Optional(Type.Number()),
-  local_protocol: Type.Optional(
-    Type.Union([Type.Literal("http"), Type.Literal("https")])
+  upstream_protocol: T.Optional(
+    T.Union([T.Literal("http"), T.Literal("https")])
   ),
-  upstream_protocol: Type.Optional(
-    Type.Union([Type.Literal("http"), Type.Literal("https")])
-  ),
-  host: Type.Optional(Type.String()),
+  host: T.Optional(T.String()),
 })
 
-const D1Binding = Type.Object({
-  binding: Type.String(),
-  database_name: Type.String(),
-  database_id: Type.String(),
-  preview_database_id: Type.Optional(Type.String()),
+const D1Binding = T.Object({
+  binding: T.String(),
+  database_name: T.String(),
+  database_id: T.String(),
+  preview_database_id: T.Optional(T.String()),
 })
 
-const DurableObjectBinding = Type.Object({
-  name: Type.String(),
-  class_name: Type.String(),
-  script_name: Type.Optional(Type.String()),
-  environment: Type.Optional(Type.String()),
+const DurableObjectBinding = T.Object({
+  name: T.String(),
+  class_name: T.String(),
+  script_name: T.Optional(T.String()),
+  environment: T.Optional(T.String()),
 })
 
-const DurableObjectMigration = Type.Object({
-  tag: Type.String(),
-  new_classes: Type.Optional(Type.Array(Type.String())),
-  renamed_classes: Type.Optional(
-    Type.Array(
-      Type.Object({
-        from: Type.String(),
-        to: Type.String(),
+const DurableObjectMigration = T.Object({
+  tag: T.String(),
+  new_classes: T.Optional(T.Array(T.String())),
+  renamed_classes: T.Optional(
+    T.Array(
+      T.Object({
+        from: T.String(),
+        to: T.String(),
       })
     )
   ),
-  deleted_classes: Type.Optional(Type.Array(Type.String())),
+  deleted_classes: T.Optional(T.Array(T.String())),
 })
 
-const KVBinding = Type.Object({
-  binding: Type.String(),
-  id: Type.String(),
-  preview_id: Type.Optional(Type.String()),
+const KVBinding = T.Object({
+  binding: T.String(),
+  id: T.String(),
+  preview_id: T.Optional(T.String()),
 })
 
-const R2BucketBinding = Type.Object({
-  binding: Type.String(),
-  bucket_name: Type.String(),
-  preview_bucket_name: Type.Optional(Type.String()),
+const R2BucketBinding = T.Object({
+  binding: T.String(),
+  bucket_name: T.String(),
+  preview_bucket_name: T.Optional(T.String()),
 })
 
-const ServiceBinding = Type.Object({
-  binding: Type.String(),
-  service: Type.String(),
-  environment: Type.Optional(Type.String()),
+const ServiceBinding = T.Object({
+  binding: T.String(),
+  service: T.String(),
+  environment: T.Optional(T.String()),
 })
 
-const TailConsumerBinding = Type.Omit(ServiceBinding, ["binding"])
+const TailConsumerBinding = T.Omit(ServiceBinding, ["binding"])
 
-const QueueProducerBinding = Type.Object({
-  binding: Type.String(),
-  queue: Type.String(),
+const QueueProducerBinding = T.Object({
+  binding: T.String(),
+  queue: T.String(),
 })
 
-const QueueConsumerBinding = Type.Object({
-  queue: Type.String(),
-  max_batch_size: Type.Optional(Type.Number()),
-  max_batch_timeout: Type.Optional(Type.Number()),
-  max_retries: Type.Optional(Type.Number()),
-  dead_letter_queue: Type.Optional(Type.String()),
-  max_concurrency: Type.Optional(Type.Number()),
+const QueueConsumerBinding = T.Object({
+  queue: T.String(),
+  max_batch_size: T.Optional(T.Number()),
+  max_batch_timeout: T.Optional(T.Number()),
+  max_retries: T.Optional(T.Number()),
+  dead_letter_queue: T.Optional(T.String()),
+  max_concurrency: T.Optional(T.Number()),
 })
 
-const AnalyticsEngineDatasetBinding = Type.Object({
-  binding: Type.String(),
-  dataset: Type.Optional(Type.String()),
+const AnalyticsEngineDatasetBinding = T.Object({
+  binding: T.String(),
+  dataset: T.Optional(T.String()),
 })
 
-const MTLSCertificateBinding = Type.Object({
-  binding: Type.String(),
-  certificate_id: Type.String(),
+const MTLSCertificateBinding = T.Object({
+  binding: T.String(),
+  certificate_id: T.String(),
 })
 
-const SendEmailBinding = Type.Union([
-  Type.Object({
-    type: Type.Literal("send_email"),
-    name: Type.String(),
-    desination_address: Type.String(),
+const SendEmailBinding = T.Union([
+  T.Object({
+    type: T.Literal("send_email"),
+    name: T.String(),
+    desination_address: T.String(),
   }),
-  Type.Object({
-    type: Type.Literal("send_email"),
-    name: Type.String(),
-    allowed_destination_addresses: Type.Array(Type.String()),
+  T.Object({
+    type: T.Literal("send_email"),
+    name: T.String(),
+    allowed_destination_addresses: T.Array(T.String()),
   }),
 ])
 
-const ConstellationBinding = Type.Object({
-  binding: Type.String(),
-  project_id: Type.String(),
+const ConstellationBinding = T.Object({
+  binding: T.String(),
+  project_id: T.String(),
 })
 
-const BrowserBinding = Type.Object({
-  binding: Type.String(),
-  type: Type.Literal("browser"),
+const BrowserBinding = T.Object({
+  binding: T.String(),
+  type: T.Literal("browser"),
 })
 
-const DispatchNamespace = Type.Object({
-  binding: Type.String(),
-  namespace: Type.String(),
-  outbound: Type.Optional(
-    Type.Object({
-      service: Type.String(),
-      parameters: Type.Array(Type.String()),
+const DispatchNamespace = T.Object({
+  binding: T.String(),
+  namespace: T.String(),
+  outbound: T.Optional(
+    T.Object({
+      service: T.String(),
+      parameters: T.Array(T.String()),
     })
   ),
 })
 
-const WorkerSite = Type.Object({
-  bucket: Type.String(),
-  include: Type.Optional(Type.Array(Type.String())),
-  exclude: Type.Optional(Type.Array(Type.String())),
+const WorkerSite = T.Object({
+  bucket: T.String(),
+  include: T.Optional(T.Array(T.String())),
+  exclude: T.Optional(T.Array(T.String())),
 })
 
-const WorkerConfig = Type.Recursive((Config) =>
-  Type.Object({
-    name: Type.String(),
-    main: Type.String(),
-    compatibility_date: Type.Union([Type.String(), Type.Date()]),
-    account_id: Type.Optional(Type.String()),
-    compatibility_flags: Type.Optional(Type.Array(Type.String())),
-    workers_dev: Type.Optional(Type.Boolean()),
-    route: Type.Optional(Route),
-    routes: Type.Optional(Type.Array(Route)),
-    tsconfig: Type.Optional(Type.String()),
-    triggers: Type.Optional(
-      Type.Object({
-        crons: Type.Array(Type.String()),
+const WorkerConfig = T.Recursive((Config) =>
+  T.Object({
+    name: T.String(),
+    main: T.String(),
+    compatibility_date: T.Union([T.String(), T.Date()]),
+    account_id: T.Optional(T.String()),
+    compatibility_flags: T.Optional(T.Array(T.String())),
+    workers_dev: T.Optional(T.Boolean()),
+    route: T.Optional(Route),
+    routes: T.Optional(T.Array(Route)),
+    tsconfig: T.Optional(T.String()),
+    triggers: T.Optional(
+      T.Object({
+        crons: T.Array(T.String()),
       })
     ),
-    usage_model: Type.Optional(
-      Type.Union([Type.Literal("Bundled"), Type.Literal("Unbound")])
+    usage_model: T.Optional(
+      T.Union([T.Literal("Bundled"), T.Literal("Unbound")])
     ),
-    rules: Type.Optional(Type.Array(Rule)),
-    build: Type.Optional(CustomBuild),
-    no_bundle: Type.Optional(Type.Boolean()),
-    minify: Type.Optional(Type.Boolean()),
-    node_compat: Type.Optional(Type.Boolean()),
-    send_metrics: Type.Optional(Type.Boolean()),
-    keep_vars: Type.Optional(Type.Boolean()),
-    logpush: Type.Optional(Type.Boolean()),
-    define: Type.Optional(Type.Record(Type.String(), Type.String())),
-    vars: Type.Optional(Type.Record(Type.String(), Type.String())),
-    d1_databases: Type.Optional(Type.Array(D1Binding)),
-    durable_objects: Type.Optional(Type.Array(DurableObjectBinding)),
-    migrations: Type.Optional(Type.Array(DurableObjectMigration)),
-    kv_namespaces: Type.Optional(Type.Array(KVBinding)),
-    r2_buckets: Type.Optional(Type.Array(R2BucketBinding)),
-    services: Type.Optional(Type.Array(ServiceBinding)),
-    queues: Type.Optional(
-      Type.Object({
-        producers: Type.Optional(Type.Array(QueueProducerBinding)),
-        consumers: Type.Optional(Type.Array(QueueConsumerBinding)),
+    rules: T.Optional(T.Array(Rule)),
+    build: T.Optional(CustomBuild),
+    no_bundle: T.Optional(T.Boolean()),
+    minify: T.Optional(T.Boolean()),
+    node_compat: T.Optional(T.Boolean()),
+    send_metrics: T.Optional(T.Boolean()),
+    keep_vars: T.Optional(T.Boolean()),
+    logpush: T.Optional(T.Boolean()),
+    define: T.Optional(T.Record(T.String(), T.String())),
+    vars: T.Optional(T.Record(T.String(), T.String())),
+    d1_databases: T.Optional(T.Array(D1Binding)),
+    durable_objects: T.Optional(T.Array(DurableObjectBinding)),
+    migrations: T.Optional(T.Array(DurableObjectMigration)),
+    kv_namespaces: T.Optional(T.Array(KVBinding)),
+    r2_buckets: T.Optional(T.Array(R2BucketBinding)),
+    services: T.Optional(T.Array(ServiceBinding)),
+    queues: T.Optional(
+      T.Object({
+        producers: T.Optional(T.Array(QueueProducerBinding)),
+        consumers: T.Optional(T.Array(QueueConsumerBinding)),
       })
     ),
-    analytics_engine_datasets: Type.Optional(
-      Type.Array(AnalyticsEngineDatasetBinding)
+    analytics_engine_datasets: T.Optional(
+      T.Array(AnalyticsEngineDatasetBinding)
     ),
-    mlts_certificates: Type.Optional(
-      Type.Array(MTLSCertificateBinding)
-    ),
-    send_email: Type.Optional(Type.Array(SendEmailBinding)),
-    tail_consumers: Type.Optional(Type.Array(TailConsumerBinding)),
-    constellation: Type.Optional(Type.Array(ConstellationBinding)),
-    browser: Type.Optional(BrowserBinding),
-    dispatch_namespaces: Type.Optional(Type.Array(DispatchNamespace)),
-    dev: Type.Optional(DevelopmentSettings),
-    site: Type.Optional(WorkerSite),
-    env: Type.Optional(
-      Type.Record(Type.String(), Type.Omit(Config, ["env"]))
-    ),
+    mlts_certificates: T.Optional(T.Array(MTLSCertificateBinding)),
+    send_email: T.Optional(T.Array(SendEmailBinding)),
+    tail_consumers: T.Optional(T.Array(TailConsumerBinding)),
+    constellation: T.Optional(T.Array(ConstellationBinding)),
+    browser: T.Optional(BrowserBinding),
+    dispatch_namespaces: T.Optional(T.Array(DispatchNamespace)),
+    dev: T.Optional(DevelopmentSettings),
+    site: T.Optional(WorkerSite),
+    env: T.Optional(T.Record(T.String(), T.Omit(Config, ["env"]))),
   })
 )
 
